d2fe227b5060435025844027555fae05af9ea0b3
legankov95 <legankov95@gmail.com>
2023-04-30 00:26:31 +0400

    fix: fix build error

---
+++ src/page/MainPage.tsx
@@ -0,0 +1,14 @@
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
+
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+      <section>Контент</section>
+      <Footer />
+    </>
+  );
+};
+
+export default MainPage;
