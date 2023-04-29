9859302d6a9723435cb88301584d6bd9e2545314
legankov95 <legankov95@gmail.com>
2023-04-29 22:00:18 +0400

    feat: add redux

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import { RouterProvider } from 'react-router-dom';
 import russian from './lang/ru';
 import english from './lang/eng';
 import router from './routes';
-import './App.css';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 const App = () => {
   const dictionary = navigator.language.includes('en') ? english : russian;
 
   return (
-    <IntlProvider messages={dictionary} locale={navigator.language} defaultLocale="ru">
-      <RouterProvider router={router} />
-    </IntlProvider>
+    <Provider store={store}>
+      <IntlProvider messages={dictionary} locale={navigator.language} defaultLocale="ru">
+        <RouterProvider router={router} />
+      </IntlProvider>
+    </Provider>
   );
 };
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
-import Root from '../page/root';
 import ErrorPage from '../page/error-page';
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import { Counter } from '../features/counter/Counter';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Root />,
+    element: <Counter />,
     errorElement: <ErrorPage />,
   },
 ]);
