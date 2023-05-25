3e17133bef814baf7fabe5c0ed7db457563ccdf1
legankov95 <legankov95@gmail.com>
2023-05-25 16:37:59 +0400

    feat: finish responsive fonts

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -7,6 +7,8 @@ const MainPage = () => {
       <Header />
       <section>
         <h1>H1 Headline</h1>
+        <h2>H2 Headline</h2>
+        <h3>H3 Headline</h3>
       </section>
       <Footer />
     </>
