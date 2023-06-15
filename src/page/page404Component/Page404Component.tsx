5829820dd2118f6f67e87cef8ad1274ee30da9ed
AlexeySkridlevskiy <skrid71@gmail.com>
2023-06-15 14:44:54 +0300

    fix page 404 all

--- src/page/page404Component/Page404Component.tsx
+++ src/page/page404Component/Page404Component.tsx
@@ -5,12 +5,13 @@ import logo404 from '../../../public/page404/404.svg';
 import AppButton from '../../components/button/AppButton';
 import { useIntl } from 'react-intl';
 
-const handleClickButton = () => {
-  const navigate = useNavigate();
-  navigate('/');
-};
-
 const Page404Component = () => {
+  const navigate = useNavigate();
+
+  const handleClickButton = () => {
+    navigate('/');
+  };
+
   const { formatMessage } = useIntl();
   return (
     <div className={style.page404_container}>
@@ -19,8 +20,10 @@ const Page404Component = () => {
       </div>
 
       <div className={classNames(style.page404_text, style.text)}>{formatMessage({ id: 'page404.text' })}</div>
-      <div className={style.page404_button} onClick={handleClickButton}>
-        <AppButton color={'primary'}>{formatMessage({ id: 'page404.button' })}</AppButton>
+      <div className={style.page404_button}>
+        <AppButton color={'primary'} onClick={handleClickButton}>
+          {formatMessage({ id: 'page404.button' })}
+        </AppButton>
       </div>
     </div>
   );
