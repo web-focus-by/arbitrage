a8cbba2e4f5d6b48dfe035d04850cac553654e85
AlexeySkridlevskiy <skrid71@gmail.com>
2023-06-15 14:37:22 +0300

    empty commit for push

--- src/page/page404Component/Page404Component.tsx
+++ src/page/page404Component/Page404Component.tsx
@@ -1,12 +1,17 @@
 import style from './Page404Component.module.scss';
 import classNames from 'classnames';
+import { useNavigate } from 'react-router-dom';
 import logo404 from '../../../public/page404/404.svg';
 import AppButton from '../../components/button/AppButton';
 import { useIntl } from 'react-intl';
 
+const handleClickButton = () => {
+  const navigate = useNavigate();
+  navigate('/');
+};
+
 const Page404Component = () => {
   const { formatMessage } = useIntl();
-  const navigate = useNavigate();
   return (
     <div className={style.page404_container}>
       <div className={style.page404_img}>
@@ -14,7 +19,7 @@ const Page404Component = () => {
       </div>
 
       <div className={classNames(style.page404_text, style.text)}>{formatMessage({ id: 'page404.text' })}</div>
-      <div className={style.page404_button} onClick={navigate('/')}>
+      <div className={style.page404_button} onClick={handleClickButton}>
         <AppButton color={'primary'}>{formatMessage({ id: 'page404.button' })}</AppButton>
       </div>
     </div>
