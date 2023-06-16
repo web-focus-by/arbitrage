520239667d831388955bf81642925e61705584e2
AlexeySkridlevskiy <skrid71@gmail.com>
2023-06-16 13:59:22 +0300

    fix files

--- src/page/mainPage/components/EarningInfoBlock/EarningInfoBlock.tsx
+++ src/page/mainPage/components/EarningInfoBlock/EarningInfoBlock.tsx
@@ -1,17 +1,17 @@
 import style from './EarningInfoBlock.module.scss';
 import classNames from 'classnames';
 import BlockTitle from '../BlockTitles/BlockTitle';
+import { useIntl } from 'react-intl';
 import imgHeadline from '../../../../../public/imgHeadline.png';
 
 const EarningInfoBlock = () => {
+  const { formatMessage } = useIntl();
   return (
     <div className={classNames(style.mainPageContainer, style.spacing_between_blocks)}>
       <div className={style.earningInfoBlockContainer}>
         <div className={style.earningInfoBlockText}>
-          <BlockTitle value={'Заработок на растущем и падающем рынке'} />
-          <div className={style.text}>
-            Куда бы не пошёл рынок, вверх или вниз, всегда есть арбитражные ситуации, на которых можно зарабатывать.
-          </div>
+          <BlockTitle value={'' + formatMessage({ id: 'earningInfoBlock.headline' })} />
+          <div className={style.text}>{formatMessage({ id: 'earningInfoBlock.text' })}</div>
         </div>
         <div className={style.earningInfoBlockImg}>
           <img src={imgHeadline} />
--- src/page/mainPage/components/OurPartners/OurPartners.tsx
+++ src/page/mainPage/components/OurPartners/OurPartners.tsx
@@ -12,45 +12,75 @@ import huobi from '../../../../../public/mainPage/ourPartners/huobi.svg';
 import gateIO from '../../../../../public/mainPage/ourPartners/gateIO.svg';
 import Bitmart from '../../../../../public/mainPage/ourPartners/Bitmart.svg';
 import Poloniex from '../../../../../public/mainPage/ourPartners/Poloniex.svg';
+import { useIntl } from 'react-intl';
+
+const dataPartners = [
+  {
+    id: 1,
+    src: Binance,
+    alt: 'Binance',
+  },
+  {
+    id: 2,
+    src: OKX,
+    alt: 'OKX',
+  },
+  {
+    id: 3,
+    src: Bybit,
+    alt: 'Bybit',
+  },
+  {
+    id: 4,
+    src: KUCOIN,
+    alt: 'KUCOIN',
+  },
+  {
+    id: 5,
+    src: Bitget,
+    alt: 'Bitget',
+  },
+  {
+    id: 6,
+    src: MEXC,
+    alt: 'MEXC',
+  },
+  {
+    id: 7,
+    src: huobi,
+    alt: 'huobi',
+  },
+  {
+    id: 8,
+    src: gateIO,
+    alt: 'gateIO',
+  },
+  {
+    id: 9,
+    src: Bitmart,
+    alt: 'Bitmart',
+  },
+  {
+    id: 10,
+    src: Poloniex,
+    alt: 'Poloniex',
+  },
+];
 
 const OurPartners = () => {
+  const { formatMessage } = useIntl();
   return (
     <div className={classNames(style.mainPageContainer, style.spacing_between_blocks)} id={'Партнеры'}>
       <div className={style.ourPartnersHeadline}>
-        <BlockTitle value={'Наши партнеры'} />
-        <AppTooltip title={'Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem'} placement={'top'} />
+        <BlockTitle value={'' + formatMessage({ id: 'ourPartners.headline' })} />
+        <AppTooltip title={'' + formatMessage({ id: 'ourPartners.tooltip' })} placement={'top'} />
       </div>
       <div className={style.OurPartnersContainer}>
-        <div className={style.OurPartnersContainerImg}>
-          <img src={Binance} alt={'BinanceLogo'} />
-        </div>
-        <div className={style.OurPartnersContainerImg}>
-          <img src={OKX} alt={'OKXLogo'} />
-        </div>
-        <div className={style.OurPartnersContainerImg}>
-          <img src={Bybit} alt={'BybitLogo'} />
-        </div>
-        <div className={style.OurPartnersContainerImg}>
-          <img src={KUCOIN} alt={'KUCOINLogo'} />
-        </div>
-        <div className={style.OurPartnersContainerImg}>
-          <img src={Bitget} alt={'BitgetLogo'} />
-        </div>
-        <div className={style.OurPartnersContainerImg}>
-          <img src={MEXC} alt={'MEXCLogo'} />
-        </div>
-        <div className={style.OurPartnersContainerImg}>
-          <img src={huobi} alt={'huobiLogo'} />
-        </div>
-        <div className={style.OurPartnersContainerImg}>
-          <img src={gateIO} alt={'gateIOLogo'} />
-        </div>
-        <div className={style.OurPartnersContainerImg}>
-          <img src={Bitmart} alt={'BitmartLogo'} />
-        </div>
-        <div className={style.OurPartnersContainerImg}>
-          <img src={Poloniex} alt={'PoloniexLogo'} />
-        </div>
+        {dataPartners.map((item) => (
+          <div key={item.id} className={style.OurPartnersContainerImg}>
+            <img src={item.src} alt={item.alt} />
+          </div>
+        ))}
       </div>
     </div>
   );
